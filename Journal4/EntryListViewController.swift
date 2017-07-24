edc1a7b48e67b6268fb48800620e1867ce1b537c
Sohailbasha <shlbsh94@gmail.com>
2017-07-24 15:34:22 -0400

    Added core data model to handle persistence. Created new cocoa touch file with a uiviewcontroller subclass for the detail view controller for each entry.

--- Journal4/EntryListViewController.swift
+++ Journal4/EntryListViewController.swift
@@ -8,20 +8,34 @@
 
 import UIKit
 
-class EntryListViewController: UIViewController {
+class EntryListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet var tableView: UITableView!
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath) as UITableViewCell
+        return cell
+    }
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
