6b95535acc40e65a6855de3bf33d6e1aeb078fbd
Sohailbasha <shlbsh94@gmail.com>
2017-07-24 15:17:28 -0400

    Added two new View Controller Objects and subclassed one as a custom view controller.

---
+++ Journal4/EntryListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EntryListViewController.swift
+//  Journal4
+//
+//  Created by Ilias Basha on 7/24/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class EntryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
