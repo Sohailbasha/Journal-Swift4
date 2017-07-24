c0006bfcf6b8d9f7367227453600828b5b93532a
Sohailbasha <shlbsh94@gmail.com>
2017-07-24 15:40:48 -0400

    adding model files for Entry. Included a core data stack and extension on Entry in order to add convenience initializers.

---
+++ Journal4/Model/CoreDataStack.swift
@@ -0,0 +1,28 @@
+//
+//  CoreDataStack.swift
+//  Journal4
+//
+//  Created by Ilias Basha on 7/24/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+enum CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Journal4")
+        
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error)")
+            }
+        })
+        return container
+    }()
+    static var context: NSManagedObjectContext { return container.viewContext }
+}
+
+
---
+++ Journal4/Model/Entry + Convenience.swift
@@ -0,0 +1,18 @@
+//
+//  Entry + Convenience.swift
+//  Journal4
+//
+//  Created by Ilias Basha on 7/24/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Entry {
+    
+    
+    
+    
+}
